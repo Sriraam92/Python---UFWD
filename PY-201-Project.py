f52911342a3eead34db346fd0eb3bd8ce1eaedc2
Sriraam C <sriraam8c@gmail.com>
2025-11-23 21:42:02 +0530

    Added PY-201 Project

---
+++ PY-201-Project.py
@@ -0,0 +1,20 @@
+import requests
+from colorama import Fore
+
+poke_name = input("Enter the Pokemon Name: ")
+poke = poke_name.upper()
+req = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke}")
+
+if req.status_code == 200:
+    data = req.json()
+    stat = data['stats']
+    print(Fore.GREEN+f"\n{poke} INFO\n")
+    print(Fore.RED+f"experience : "+Fore.CYAN+f"{data['base_experience']}")
+    for item in stat:
+        val = item['base_stat']
+        stat_name = item['stat']['name']
+        print(Fore.RED+f"{stat_name} : "+Fore.CYAN+f"{val}"+Fore.WHITE)
+    print("")
+else:
+    print(Fore.RED+"\nInvalid Pokemon Entered\n"+Fore.WHITE)
+
