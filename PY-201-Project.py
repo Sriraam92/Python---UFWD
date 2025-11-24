72a9746c25b31a8f6eb08b92a049f71e86013587
Sriraam C <sriraam8c@gmail.com>
2025-11-24 21:32:41 +0530

    Modified GET Request Syntax in  PY-201 Project

--- PY-201-Project.py
+++ PY-201-Project.py
@@ -2,13 +2,13 @@ import requests
 from colorama import Fore
 
 poke_name = input("Enter the Pokemon Name: ")
-poke = poke_name.upper()
+poke = poke_name.lower()
 req = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke}")
 
 if req.status_code == 200:
     data = req.json()
     stat = data['stats']
-    print(Fore.GREEN+f"\n{poke} INFO\n")
+    print(Fore.GREEN+f"\n{poke.upper()} INFO\n")
     print(Fore.RED+f"experience : "+Fore.CYAN+f"{data['base_experience']}")
     for item in stat:
         val = item['base_stat']
